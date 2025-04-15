2664114cb3f1ace4bdc7f9205643e30d2c484583
best <2907647445@qq.com>
2025-04-15 20:01:12 +0800

    Button组件编码

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+]);
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from "./components/Button/Button.vue";
+</script>
+
+<template>
+  <Button type="primary" plain>Test</Button>
+</template>
+
+<style scoped>
+</style>
