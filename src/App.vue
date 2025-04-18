b8eb02c74bc1d7f43035bd3b0ac02b76fff8f245
best <2907647445@qq.com>
2025-04-18 16:53:54 +0800

    postcss色彩系统

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@ import Button from "./components/Button/Button.vue";
 
 <template>
   <Button type="primary" plain>Test</Button>
+  <Button type="success">Success</Button>
+  <Button type="info">Info</Button>
+  <Button type="warning">Warning</Button>
+  <Button type="danger" disabled>Danger</Button>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
