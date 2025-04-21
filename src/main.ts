cfcae0a9993e7c35cb2fa9834862ba7e9c50f1fe
best <2907647445@qq.com>
2025-04-21 20:52:42 +0800

    peng

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import './styles/index.css'
 
-createApp(App).mount('#app')
+library.add(fas)
+
+const app = createApp(App)
+
+
+app.mount('#app')
